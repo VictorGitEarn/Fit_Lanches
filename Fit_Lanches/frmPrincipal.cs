2b792ed535e1a31f7b1f66b26c2fc0002f2e2231
Victor Alves <victor.earn@outlook.com>
2019-02-24 15:00:01 -0300

    Adicionando comentários finais

--- Fit_Lanches/frmPrincipal.cs
+++ Fit_Lanches/frmPrincipal.cs
@@ -37,9 +37,9 @@ namespace Fit_Lanches
         private void tmrHora_Tick(object sender, EventArgs e)
         {
             lblAtual.Text = "Dia e Hora atual: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-            // lblteste.Text = prp_Count.ToString();
         }
 
+        //Botão para mandar o pedido para cozinha
         private void btnPrep_Click(object sender, EventArgs e)
         {
             //Verifica se tem nome.
@@ -50,9 +50,9 @@ namespace Fit_Lanches
             }
             else
             {
+                //Verificar se a cozinha está cheia, caso estiver 02, adicionar pedido para lista de espera
                 if (prp_Count == 2)
                 {
-                    //adiciona o pedido para espera
                     ListViewItem itmEspera = new ListViewItem(txtNome.Text);
                     itmEspera.SubItems.Add(listItem.SelectedItems[0].Text);
                     itmEspera.SubItems.Add(listItem.SelectedItems[0].SubItems[2].Text);
@@ -67,18 +67,89 @@ namespace Fit_Lanches
                         prp_Count++;
                     }
                 }
+                //Reseta ambiente de pedido
                 txtNome.Text = "";
                 txtNome.Focus();
                 btnPrep.Enabled = false;
             }
         }
 
-        //Métodos gerenciar a cozinha e listas
+        /*Proceso dos contadores (preparação), assim que pedido for finalizado,
+         * será feito a verificação se tem item na espera e envia 
+         * o item pronto para a lista de entrega.*/
+        private void tmrProcess_Tick(object sender, EventArgs e)
+        {
+            durProcess1--;
+            lblCount1.Text = "Tempo (Seg): " + durProcess1;
+            picPrep11.Visible = true;
+            picPrep12.Visible = false;
+            //Quando o pedido ficar pronto na cozinha, preparo 1...
+            if (durProcess1 == 0)
+            {
+                picPrep11.Visible = false;
+                picPrep12.Visible = true;
+                prp_Count--;
+                tmrProcess.Stop();
+                //Preenche a retirada
+                ListViewItem itemlista = new ListViewItem(lblPrepNome1.Text);
+                itemlista.SubItems.Add(lblPrepITem1.Text);
+                listEntrega.Items.Add(itemlista);
+                //Limpa o preparo
+                finaliza(lblPrepNome1, lblPrepITem1);
+                //Metodo para puxar se tiver um item em espera
+                if (listEspera.Items.Count > 0)
+                {
+                    puxEspera();
+                    listEspera.Items.Remove(listEspera.Items[0]);
+                    prp_Count++;
+                }
+            }
+        }
+
+        private void tmrProcess2_Tick(object sender, EventArgs e)
+        {
+            durProcess2--;
+            lblCount2.Text = "Tempo (Seg): " + durProcess2;
+            picPrep21.Visible = true;
+            picPrep22.Visible = false;
+            //Quando o pedido ficar pronto na cozinha, preparo 2...
+            if (durProcess2 == 0)
+            {
+                picPrep21.Visible = false;
+                picPrep22.Visible = true;
+                prp_Count--;
+                tmrProcess2.Stop();
+                //Preenche a retirada
+                ListViewItem itemlista = new ListViewItem(lblPrepNome2.Text);
+                itemlista.SubItems.Add(lblPrepITem2.Text);
+                listEntrega.Items.Add(itemlista);
+
+                //Limpa o preparo
+                finaliza(lblPrepNome2, lblPrepITem2);
+                //Metodo para puxar se tiver um item em espera
+                if (listEspera.Items.Count > 0)
+                {
+                    puxEspera();
+                    listEspera.Items.Remove(listEspera.Items[0]);
+                    prp_Count++;
+                }
+            }
+        }
+
+        //Habilita o botão para preparar, somente se tiver um item selecionado.
+        private void listItem_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            btnPrep.Enabled = true;
+        }
+
+        //Métodos
 
         //Lança um pedido para a Cozinha puxando informações direto do cardápio.
         public void prepComum()
         {
-            //Verificar o contador de preparo para não passar de 02, ou se já tiver um item sendo preparado, não parar a cozinha por conta do contador.
+            /*Verificar o contador se a cozinha está vazia ou com algum item sendo preparado, 
+             * se já tiver um item sendo preparado mas no preparo 02, 
+             * foi colocado o ou para o preparo 01 não ficar esperando o contador ficar menor que 01.*/
             if (prp_Count < 1 || prp_Count == 1 && tmrProcess2.Enabled == true)
             {
                 durProcess1 = Convert.ToInt32(listItem.SelectedItems[0].SubItems[2].Text);
@@ -89,6 +160,7 @@ namespace Fit_Lanches
                 lblPrepNome1.Visible = true;
                 tmrProcess.Start();
             }
+            //Passará para o preparo 02, se o preparo 01 estiver ocupado.
             else
             {
                 durProcess2 = Convert.ToInt32(listItem.SelectedItems[0].SubItems[2].Text);
@@ -125,6 +197,7 @@ namespace Fit_Lanches
                 tmrProcess2.Start();
             }
         }
+        
         // Método para limpar os preparos
         void finaliza(Label nome, Label item)
         {
@@ -132,64 +205,25 @@ namespace Fit_Lanches
             item.Visible = false;
 
         }
-        //Proceso dos contadores
-        private void tmrProcess_Tick(object sender, EventArgs e)
+        
+        //Carregar txt com o cardápio
+        private void btnLoadCard_Click(object sender, EventArgs e)
         {
-            durProcess1--;
-            lblCount1.Text = "Tempo (Seg): " + durProcess1;
-            picPrep11.Visible = true;
-            picPrep12.Visible = false;
-            //Quando o pedido ficar pronto na cozinha, preparo 1...
-            if (durProcess1 == 0)
-            {
-                picPrep11.Visible = false;
-                picPrep12.Visible = true;
-                prp_Count--;
-                tmrProcess.Stop();
-                //Preenche a retirada
-                ListViewItem itemlista = new ListViewItem(lblPrepNome1.Text);
-                itemlista.SubItems.Add(lblPrepITem1.Text);
-                listEntrega.Items.Add(itemlista);
-                //Limpa o preparo
-                finaliza(lblPrepNome1, lblPrepITem1);
-                //Metodo para puxar se tiver um item em espera
-                if (listEspera.Items.Count > 0)
-                {
-                    puxEspera();
-                    listEspera.Items.Remove(listEspera.Items[0]);
-                    prp_Count++;
-                }
-            }
+            carregTxt();
         }
-        private void tmrProcess2_Tick(object sender, EventArgs e)
-        {
-            durProcess2--;
-            lblCount2.Text = "Tempo (Seg): " + durProcess2;
-            picPrep21.Visible = true;
-            picPrep22.Visible = false;
-            //Quando o pedido ficar pronto na cozinha, preparo 2...
-            if (durProcess2 == 0)
-            {
-                picPrep21.Visible = false;
-                picPrep22.Visible = true;
-                prp_Count--;
-                tmrProcess2.Stop();
-                //Preenche a retirada
-                ListViewItem itemlista = new ListViewItem(lblPrepNome2.Text);
-                itemlista.SubItems.Add(lblPrepITem2.Text);
-                listEntrega.Items.Add(itemlista);
 
-                //Limpa o preparo
-                finaliza(lblPrepNome2, lblPrepITem2);
-                //Metodo para puxar se tiver um item em espera
-                if (listEspera.Items.Count > 0)
-                {
-                    puxEspera();
-                    listEspera.Items.Remove(listEspera.Items[0]);
-                    prp_Count++;
-                }
+        //Botão de entrega
+        private void btnEntreg_Click(object sender, EventArgs e)
+        {
+            //Verificar item selecionado e alterar o item para entregue, alterando a Font e cor de background
+            ListViewItem item = listEntrega.SelectedItems[0];
+            if (item.Font.Bold)
+            {
+                listEntrega.SelectedItems[0].Font = new Font(listEntrega.Font, FontStyle.Regular);
+                listEntrega.SelectedItems[0].BackColor = Color.Green;
             }
         }
+
         //carregar Txt
         public void carregTxt()
         {
@@ -203,7 +237,7 @@ namespace Fit_Lanches
                     while (!reader.EndOfStream)
                     {
                         string linha = reader.ReadLine();
-
+                        //Separa os dados das colunas com o |
                         string[] valores = linha.Split('|');
 
                         nomes.Add(valores[0]);
@@ -219,25 +253,6 @@ namespace Fit_Lanches
                     itemLista.SubItems.Add(tempos[i].ToString());
                     listItem.Items.Add(itemLista);
                 }
-            }
-        }
-        private void listItem_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
-        {
-            btnPrep.Enabled = true;
-        }
-
-        private void btnLoadCard_Click(object sender, EventArgs e)
-        {
-            carregTxt();
-        }
-
-        private void btnEntreg_Click(object sender, EventArgs e)
-        {
-            ListViewItem item = listEntrega.SelectedItems[0];
-            if (item.Font.Bold)
-            {
-                listEntrega.SelectedItems[0].Font = new Font(listEntrega.Font, FontStyle.Regular);
-                listEntrega.SelectedItems[0].BackColor = Color.Green;
             }
         }
     }
